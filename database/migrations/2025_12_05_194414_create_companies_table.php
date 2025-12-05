213e498401375e6462ba3d966c74803c9de16a61
OswaCano <braya@LAPTOP-BOIOCOPR.localdomain>
2025-12-05 13:49:22 -0600

    migrations companies & registered agents

---
+++ database/migrations/2025_12_05_194414_create_companies_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('name');
+            $table->string('state')->index();
+            $table->enum('registration_agent_type', ['user', 'registered_agent'])->default('user');
+            $table->foreignId('registration_agent_id')->nullable()->constrained('registered_agents')->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
